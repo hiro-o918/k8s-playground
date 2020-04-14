8b269e88c189846615aaa36eac558e4de5f4d357
Hironori Yamamoto <hironori.yamamoto@gunosy.com>
2020-04-14 17:49:31 +0900

    Update deploy code

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+# build and push images
+docker build -t $DOKERHUB_ID/k8s-complex-client:latest -t $DOKERHUB_ID/k8s-complex-client:$SHA -f ./client/Dockerfile .
+docker build -t $DOKERHUB_ID/k8s-complex-worker:latest -t $DOKERHUB_ID/k8s-complex-worker:$SHA -f ./worker/Dockerfile .
+docker build -t $DOKERHUB_ID/k8s-complex-server:latest -t $DOKERHUB_ID/k8s-complex-server:$SHA -f ./server/Dockerfile .
+echo "$DOKERHUB_LOGIN_KEY" | docker login -u "$DOKERHUB_ID" --password-stdin
+docker push $DOKERHUB_ID/k8s-complex-client:latest
+docker push $DOKERHUB_ID/k8s-complex-worker:latest
+docker push $DOKERHUB_ID/k8s-complex-server:latest
+docker push $DOKERHUB_ID/k8s-complex-client:$SHA
+docker push $DOKERHUB_ID/k8s-complex-worker:$SHA
+docker push $DOKERHUB_ID/k8s-complex-server:$SHA
+
+# configre kubectl
+aws eks --region ap-northeast-1 update-kubeconfig --name hironori-udemy-k8s-cluster
+# apply manufests
+kubectl apply -f k8s --dry-run=client
+kubectl set image deployments/client-deployment client=$DOKERHUB_ID/k8s-complex-client:$SHA --dry-run=client
+kubectl set image deployments/worker-deployment worker=$DOKERHUB_ID/k8s-complex-client:$SHA --dry-run=client
+kubectl set image deployments/server-deployment server=$DOKERHUB_ID/k8s-complex-client:$SHA --dry-run=client
